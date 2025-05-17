d697e3452f5cb59bdd704221a649c453ae7910c0
Boluwatife Johnson <jaycewap@gmail.com>
2025-05-17 22:37:34 +0100

    Refactor code structure for improved readability and maintainability

--- src/assets/scripts/card.ts
+++ src/assets/scripts/card.ts
@@ -1,16 +1,21 @@
-import { animate, hover, stagger } from "motion"
+import { animate, hover, stagger } from "motion";
 
 export function animateCard() {
-  const cards = document.querySelectorAll(".card")!
+  const cards = document.querySelectorAll(".card")!;
 
   cards.forEach((card) => {
-    const li = card.querySelectorAll("li")!
-    const ul = card.querySelector("ul")!
-    const h3 = card.querySelector("h3")!
-    const span = card.querySelector("span")!
-    const a = card.querySelector("a")!
+    const li = card.querySelectorAll("li")!;
+    const ul = card.querySelector("ul")!;
+    const h3 = card.querySelector("h3")!;
+    const span = card.querySelector("span")!;
+    const a = card.querySelector("a")!;
 
-    animate(span, { display: "none", opacity: 0 }, { duration: .3, ease: 'circOut' })
+    animate(
+      span,
+      { display: "none", opacity: 0 },
+      { duration: 0.3, ease: "circOut" }
+    );
+    animate(a, { width: "48px" }, { duration: 0.3, ease: "circOut" });
 
     animate(
       li,
@@ -18,51 +23,78 @@ export function animateCard() {
       { duration: 0.3, delay: stagger(0.03), easing: "easeIn" }
     );
 
-    animate(ul, {
-      display: 'none',
-    },
+    animate(
+      ul,
       {
-        duration: .3,
-        ease: 'easeOut'
-      })
-    animate(h3, { y: ["0%", "5%"] })
+        display: "none",
+      },
+      {
+        duration: 0.3,
+        ease: "easeOut",
+      }
+    );
+    animate(h3, { y: ["0%", "5%"] });
 
     hover(card, (el) => {
-      animate(a, { width: "auto" }, { duration: 3, ease: 'circIn' })
-      animate(h3, { y: ["50%", "0%"] })
-      animate(ul, {
-        display: 'block',
-      },
+      animate(a, { width: "auto" }, { duration: 3, ease: "circOut" });
+      animate(h3, { y: ["50%", "0%"] }, { duration: 0.3, ease: "circOut" });
+      animate(
+        ul,
         {
-          duration: .6,
-          ease: 'easeOut'
-        })
+          display: "block",
+          opacity: [0, 1],
+          y: [10, 0],
+        },
+        {
+          duration: 0.6,
+          ease: "easeOut",
+        }
+      );
       animate(
         li,
         { opacity: [0, 1], y: [10, 0] },
         { duration: 0.4, delay: stagger(0.05), easing: "easeOut" }
       );
 
-      animate(span, { display: "flex", opacity: 1 }, { duration: .4, ease: 'circOut' })
+      animate(
+        span,
+        { display: "flex", opacity: 1 },
+        { duration: 0.4, ease: "circOut" }
+      );
+      animate(
+        a,
+        { width: "150px", display: "flex" },
+        { duration: 0.3, ease: "circOut" }
+      );
 
       return () => {
-        animate(span, { display: "none", opacity: 0 }, { duration: .3, ease: 'circOut' })
+        animate(a, { width: "48px" }, { duration: 0.3, ease: "circOut" });
+        animate(
+          span,
+          { display: "none", opacity: 0 },
+          { duration: 0.3, ease: "circOut" }
+        );
 
         animate(
           li,
           { opacity: [1, 0], y: [0, 10] },
           { duration: 0.3, delay: stagger(0.03), easing: "easeIn" }
-        );
+        )
 
-        animate(ul, {
-          display: 'none',
-        },
+        animate(
+          ul,
           {
-            duration: .3,
-            ease: 'easeOut'
-          })
-        animate(h3, { y: ["0%", "5%"] })
-      }
-    })
-  })
+            display: "none",
+            opacity: [1, 0],
+            y: [0, 10],
+          },
+          {
+            duration: 0.3,
+            ease: "circOut",
+          }
+        );
+        animate(h3, { y: ["0%", "5%"] }, { duration: 0.3, ease: "circOut" });
+      };
+    });
+  });
 }
